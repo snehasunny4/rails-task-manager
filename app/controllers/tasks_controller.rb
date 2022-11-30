cb1d54d837446eb682c57a249385c43924b53f1b
Sneha Sunny <snehasunny1997@gmail.com>
2022-11-30 23:40:52 +0800

    Added Bootstrap to the code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :delete]
   def index
     @tasks = Task.all
   end
 
   def show
-    @tasks = Task.find(params[:id])
   end
 
   def new
@@ -12,28 +12,27 @@ class TasksController < ApplicationController
   end
 
   def create
-    @tasks = Task.new(task_params)
     @tasks.save
   end
 
   def edit
-    @tasks = Task.find(params[:id])
   end
 
   def update
-    @tasks = Task.find(params[:id])
     @tasks.update(task_params)
   end
 
   def delete
-    @tasks = Task.find(params[:id])
     @tasks.destroy
   end
 end
-
 
 private
 
 def task_params
   params.require(:task).permit(:title, :details, :completed)
+end
+
+def set_task
+  @tasks = Task.find(params[:id])
 end
